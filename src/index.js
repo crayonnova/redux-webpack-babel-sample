46283bebfa36c030dcd7b424730caa87063c63dc
emo lay <julyemolay77@gmail.com>
2020-02-18 09:20:10 +0630

    create with input validation and preventing from store change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
-
 const btnSubmit = document.getElementById('submit');
 const txtInput  = document.getElementById('txtInput');
 const list      = document.getElementById('list');
@@ -23,6 +23,7 @@ const deleteTodo = (data) => {
 }
 
 const initialState = {
+    error   : false,
     loading : false,
     list    : [],
 }
@@ -32,13 +33,35 @@ const todoReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TODO:
             
-            return {
-                ...state,
-                list : [...state.list,{
-                    task    : action.payload,
-                    status  : false
-                }]
+            const isExist = state.list.find( todo => todo.task === action.payload)
+            if(action.payload==''){
+
+                console.log('empty')
+
+                return {
+                    ...state,
+                    error : true
+                };
             }
+            else if(isExist){
+                console.log('Already exist')
+                return {
+                    ...state,
+                    error : true
+                };
+            }
+            else{
+
+                return {
+                    ...state,
+                    error : false,
+                    list : [...state.list,{
+                        task    : action.payload,
+                        status  : false
+                    }]
+                }
+            }
+
            
         default:
            return state
@@ -46,17 +69,21 @@ const todoReducer = (state = initialState, action) => {
 }
 
 const render = () => {
+    console.log('....RENDERED..')
     var data = '';
     const store = todoStore.getState();
-    store.list.forEach(todo => {
-         data += `<li> ${todo.task} </li>`;
+    store.list.forEach((todo,index) => {
+         data += `<li key=${index}> ${todo.task} </li>`;
     });
+
     list.innerHTML = data
 }
 
-const todoStore = createStore(todoReducer)
+const todoStore = createStore(todoReducer,applyMiddleware(createLogger()))
 const unsubscribe = todoStore.subscribe(() => {
+    if(!todoStore.getState().error)
     render();
+
 })
 
 
